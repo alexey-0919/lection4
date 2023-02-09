45bd29ad668b5df610fe40465b69360789b55faa
alehey <alexey.p.svyz@mail.ru>
2023-02-09 17:03:46 +0700

    ad task3

---
+++ task3/Program.cs
@@ -0,0 +1,25 @@
+﻿/* double Factorial (int n)
+{
+    if (n == 1) return 1; //1! = 1; 0! = 1
+    else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}" ); // 1*2*3=6
+} */
+
+
+// f(1) = 1
+// f2) = 1
+// F(n) = f(n-1) + f(n-2)
+
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f{i} = {Fibonacci(i)}");
+} 
