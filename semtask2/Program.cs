65b44ed68ea732c9cbf97b4bf7bcdddac44d146c
alehey <alexey.p.svyz@mail.ru>
2023-02-13 15:26:15 +0700

    add semtask2

---
+++ semtask2/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+/* for (int N = 100; N < 1000; N++)
+{
+if (N*N % 1000 == N)
+Console.WriteLine($" {N} {N*N} ");
+} */
+
+for (int N = 10; N < 1000; N++)
+{
+if (N*N % 10 == N)
+Console.WriteLine($" {N} {N*N} ");
+}
